5c3da0ce74ff11ae73403382740f28fe74f747e5
ajaps <ajapsfranklin@gmail.com>
2019-10-06 20:14:59 +0100

    - upgrade to python 3

--- get_image_text.py
+++ get_image_text.py
@@ -7,25 +7,26 @@ import cv2
 import os
 import numpy as np
 import io
-import urllib2 as urllib
+from urllib.request import urlopen
+
 
 class Image_Ocr:
-  def __init__(self, image_path):
-    self.image_path = image_path
+    def __init__(self, image_path):
+        self.image_path = image_path
 
-  def get_text_from_image(self):
-    custom_oem_psm_config = ''
+    def get_text_from_image(self):
+        custom_oem_psm_config = ''
 
-    try:
-      fd = urllib.urlopen(self.image_path)
-      image_file = io.BytesIO(fd.read())
+        try:
+            fd = urlopen(self.image_path).read()
+            image_file = io.BytesIO(fd)
 
-      text = pytesseract.image_to_string(
-        Image.open(image_file), lang='eng', config=custom_oem_psm_config
-      )
-    except Exception as e:
-      return {'error': e.message}
+            text = pytesseract.image_to_string(
+                Image.open(image_file), lang='eng', config=custom_oem_psm_config
+            )
+        except Exception as e:
+            return {'error': e.message}
 
-    return {'data': text}
+        return {'data': text}
 
 # python get-all-text.py --image 'images/page_1.jpg'
